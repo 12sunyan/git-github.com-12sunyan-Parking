37a6f5cc2e136831b8be27c904ced831388563aa
12sunyan <sunyan940317@163.com>
2016-11-21 20:36:08 +0800

    登录注册界面1.0

--- app/www/js/register.js
+++ app/www/js/register.js
@@ -1,5 +1,6 @@
 angular.module('parking').controller('RegisterCtrl', function ($location,$state, $scope, $http) {
   $scope.register = function() {
-    $state.go('main.home');
+  	alert("注册成功");
+    $state.go('login');
   }
 });
